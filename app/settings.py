134c826c427dacf3960706bc724c3e2b44823b37
Arduinum628 <message.chaos628@gmail.com>
2025-08-11 18:54:28 +0300

    feat: created user model, create migtation for user, configured alembic

---
+++ app/settings.py
@@ -0,0 +1,38 @@
+from pydantic import SecretStr  
+from pydantic_settings import BaseSettings, SettingsConfigDict 
+
+
+class ModelConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file = '.env', 
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+
+class SettingsDb(ModelConfig):
+    """Класс для данных бд"""
+
+    type_and_driver_db: str
+    name_db: str
+    user_db: str
+    password_db: SecretStr
+    host_db: str
+    port_db: int
+    echo_db: bool
+
+    @property  
+    def get_url_db(self):  
+        """Метод вернёт url для подключения бд"""
+
+        return (f'{self.type_and_driver_db}://{self.user_db}:{self.password_db.get_secret_value()}'
+                f'@{self.host_db}:{self.port_db}/{self.name_db}')
+    
+
+class Settings(ModelConfig):
+    """Класс для данных конфига"""
+    
+    db_settings: SettingsDb = SettingsDb()
+
+
+settings = Settings()
